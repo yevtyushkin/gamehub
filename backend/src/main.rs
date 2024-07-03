71b830e3dcd547f61958fcc00cce169ef4d0d9c3
Daniyil Yevtyushkin <48154732+yevtyushkin@users.noreply.github.com>
2024-07-03 22:46:34 +0100

    :sparkles: Implement basic env-based configuration (#6)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use crate::config::application_config::ApplicationConfig;
+
+mod config;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let application_config = ApplicationConfig::from_environment()?;
+
+    Ok(())
 }
