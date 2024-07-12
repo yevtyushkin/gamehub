c0fe6c2004bb268c25693af857365dc200b80a6b
Daniyil Yevtyushkin <48154732+yevtyushkin@users.noreply.github.com>
2024-07-12 18:26:32 +0100

    :building_construction: Use `Uuid-v7` for `PlayerId` (#13)
    * :building_construction: Use `Uuid-v7` for `PlayerId`
    
    * Remove unused feature

--- backend/src/player/player_id.rs
+++ backend/src/player/player_id.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
-use ulid::Ulid;
+use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
 /// Represents a player's ID.
-pub struct PlayerId(pub Ulid);
+pub struct PlayerId(pub Uuid);
 
 impl PlayerId {
     /// Creates a random [PlayerId].
     pub fn new() -> PlayerId {
-        PlayerId(Ulid::new())
+        PlayerId(Uuid::now_v7())
     }
 }
